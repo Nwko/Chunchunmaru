6a7808c5d67fdc4e791c4d84930df263fc325480
Nwko <83485955+Nwko@users.noreply.github.com>
2022-05-07 13:22:54 +0545

    e

--- TODO.md
+++ TODO.md
@@ -5,6 +5,7 @@
 - [ ] Make the ping command work
 - [ ] Make a bulk delete command
 - [ ] Idk
+- [ ] Add more functionality to the help command
 
 ## Completed
 
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,26 +1,26 @@
 import { Pagination } from '@discordx/pagination';
-import type { CommandInteraction, ContextMenuInteraction } from 'discord.js';
-import { MessageEmbed } from 'discord.js';
-import { Discord, MetadataStorage, Slash, ContextMenu } from 'discordx';
+import type { CommandInteraction } from 'discord.js';
+import { MessageEmbed, Guild } from 'discord.js';
+import { Discord, MetadataStorage, Slash } from 'discordx';
 
 @Discord()
 export class help {
-    @Slash("help", { description: "Need help? No Worries!" })
+    @Slash("help", { description: "Need help? No Worries! (Probably :D)" })
     async pages(interaction: CommandInteraction): Promise<void> {
         const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
             return { description: cmd.description, name: cmd.name };
         });
 
-        const pages = commands.map((cmd, i) => {
+        const main = commands.map((cmd, i) => {
             return new MessageEmbed()
                 .setColor("#bdd3f5")
                 .setFooter({ text: `Page ${i + 1} of ${commands.length}` })
-                .setTitle("**Here to help!**")
+                .setTitle("**Here to help! :D**")
                 .addField("Command", cmd.name)
                 .addField("Description", cmd.description);
         });
 
-        const pagination = new Pagination(interaction, pages);
+        const pagination = new Pagination(interaction, main);
         await pagination.send();
     }
 }
